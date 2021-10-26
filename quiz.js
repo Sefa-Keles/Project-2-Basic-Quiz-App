4117d68a50ab11b4e7cfbce1a798e095ad730f6b
Sefa Keles <sefaaakeles@gmailcom>
2021-10-26 15:11:59 +0100

    paging and restart button added

--- quiz.js
+++ quiz.js
@@ -1,112 +1,143 @@
 
-//Question Constructor
-
-let Question = function(text, options, answer){ // Buradaki answer soru nesnesinden gelen answer. Benim sectigim cevap olan answer degil. O guess metodunun icerisinde
+//QUESTION CONSTRUCTOR
+let Question = function(text, options, answer,comment){
     this.text = text;
     this.options = options;
     this.answer = answer;
+    this.comment = comment;
 }
 
-Question.prototype.checkAnswer = function(answer){
-     return this.answer === answer;       
+Question.prototype.checkAnswer = function(selectedElement){
+    let parent = selectedElement.parentNode;
+    if(this.answer === selectedElement.innerText){
+        $(parent).attr("class", "btn btn-success");
+        $(parent).siblings().attr ("disabled", "disabled");
+        return true;
+    }else {
+        $(parent).attr("class", "btn btn-danger");
+        $(parent).siblings().attr ("disabled", "disabled");
+        let  infoHtml= `<br><p>${this.comment}</p>`
+        document.getElementById("solutionInfo").innerHTML = infoHtml;
+    }  
 }
 
-//Quiz Constructor
-
-function Quiz(questions){ // diziler gelecek
+//QUIZ CONSTRUCTOR
+function Quiz(questions){
     this.questions = questions;
     this.score = 0;
-    this.questionIndex = 0; // Gelen soru dizisini baslatmak icin kullanacagim sayac. Sorunun hangi soru oldugunu ayarlayacak.
+    this.questionIndex = 0; 
 }
 
-Quiz.prototype.getQuestion = function(){ // Index numarasina gore sorulari getirecek fonksiyondur
+Quiz.prototype.getQuestion = function(){ 
     let nextQuestion = this.questions[this.questionIndex];
-    //console.log("getQuestion :",nextQuestion)
     return nextQuestion;
     
 }
 
 Quiz.prototype.isFinish = function(){
-    return this.questions.length === this.questionIndex;
-}
-
-Quiz.prototype.guess = function (answer){ // Burada yaptigim sey quize sectigim cevabimi gondermem gerekiyor. Bu yolladigim cevabin sonucunda ne olacagini ayarliyorum.
-    let myQuestion = this.getQuestion();
-
-    if(myQuestion.checkAnswer(answer)){
-        this.score++;
-        console.log("Dogru cevap")
-    }
-
-    this.questionIndex++;
-    //console.log(myQuestion)
+    return this.questions.length === this.questionIndex
+        
     
 }
 
-//Yani kisacasi Constructorlari olustur. Soruyu goster. Cevabimi al ve bu cevaba gore gereken islemleri yap. Quiz bittimi sorgula...
-
-let q1 =  new Question("How many degrees are the interior angles of the triangle?", ["90", "180", "270", "360"], "180");
-let q2 =  new Question("0,1,1,2,3,5,8,13,21,x   What number could x be?", ["29", "32", "34", "43"], "34");
-let q3 =  new Question("24-4x3/(8+4) = ?", ["0", "5", "8", "23"], "23");
-
-var questions = [q1, q2, q3];
-
-// Start Quiz
-
-let quiz = new Quiz(questions);
-
-function loadQuestion(){
-    if(quiz.isFinish()){
-        showScore();
-    }else{
-        let quizQuestion = quiz.getQuestion();
-        let quizOptions = quizQuestion.options;
-        //console.log(quizOptions)
-        document.querySelector("#question").textContent = quizQuestion.text; //Soru elemente yuklendi.
-
-        for(let i=0; i<quizOptions.length; i++){
-            let optionElement = document.querySelector("#option"+i);
-            optionElement.innerText = quizOptions[i];   
-            getSelection("btn"+i, quizOptions[i]); //Bu fonksiyon ile for'dan donen butun butonlari ve secimleri disaridaki fonksiyona parametre olarak yollayacagim.
-        }
-
-        showProgress();
+Quiz.prototype.guess = function (selectedElement){ 
+    let myQuestion = this.getQuestion();
+    if(myQuestion.checkAnswer(selectedElement)){
+        this.score++;
     }
 }
 
+let q1 = new Question("1 What is the best programming language", ["php", "C#", "JS", "Phyton"], "C#","C# is a Microsoft product and Microsoft products are the most used products in the world.");
+let q2 = new Question("2 What is the most popular programming language", ["php", "C#", "JS", "Phyton"], "JS", "The most up-to-date technologies on web pages are produced based on Javascript.");
+let q3 = new Question("3 What is the best modern programming language", ["php", "C#", "JS", "Phyton"], "Phyton", "The latest technology used in artificial intelligence programming is Python");
 
+let questions = [q1, q2, q3];
+let quiz = new Quiz(questions);
 
-loadQuestion();
+//LOAD ELEMENTS TO HTML
+function loadQuestion(){
+    let nextButton = document.getElementById("btnNext");
+    let previousButton = document.getElementById("btnPrevious");
+    if(quiz.isFinish()){
+        showScore(previousButton, nextButton);
+    }else{  
+        let quizQuestion = quiz.getQuestion();
+        let quizOptions = quizQuestion.options;
+        document.querySelector("#question").innerText = quizQuestion.text;
+        for(let i=0; i<quizOptions.length; i++){
+            let optionElement = document.querySelector("#option"+i);
+            let parentElements = optionElement.parentElement;
+            optionElement.innerText = quizOptions[i];   
+            getSelection("btn"+i, optionElement); 
+            previousQuestion(previousButton, parentElements);
+            nextQuestion(nextButton, parentElements);
+        }
+        showProgress();
+    }  
+}
 
-// Buttonlarin hepsi array olarak burda duruyor. button degiskenine onclick atadigimda otomatik olarak arrayin tumune onclick atanacak ve
-//otomatik olarak hangisi tiklanirsa onun onclick ozelligi tetiklenecegi icin parametre olarak o quess metoduna gonderilecek.
-function getSelection(id, option){
-    let button = document.getElementById(id); 
-    console.log(button)
-    button.onclick = function(){
-        quiz.guess(option);
-        loadQuestion();
+function getSelection(id, optionElement){
+    let button = document.getElementById(id);
+    button.onclick = function(){ 
+        quiz.guess(optionElement);
     };
 };
 
-function showScore(){
-    let html  = `<h2>Score</h2><h4>${quiz.score}</h4>`;
-    document.querySelector(".card-body").innerHTML = html;
+function previousQuestion(btn, btnOptions){
+    document.getElementById("solutionInfo").innerHTML ="";
+    btnOptions.setAttribute("class", "btn btn-primary");
+    btnOptions.removeAttribute("disabled");
+    if(quiz.questionIndex !== 0){
+        btn.style.display = "block";
+        btn.onclick = function(){
+            quiz.questionIndex--;
+            loadQuestion();
+        }
+    }else {
+        btn.style.display = "none";
+    }
+
+}
+
+
+function nextQuestion(btnNext,btnOptions){
+    document.getElementById("solutionInfo").innerHTML ="";
+    btnOptions.setAttribute("class", "btn btn-primary");
+    btnOptions.removeAttribute("disabled");
+    if(quiz.questionIndex == 0){
+        btnNext.style.display ="block";
+        btnNext.onclick = function(){
+            quiz.questionIndex++;
+            loadQuestion();
+        };
+    }
+    
+};
+
+function showScore(...buttons){
+    for(let i=0; i<buttons.length; i++){
+        buttons[i].style.display = "none";
+    };
+    let html  = `<h2>Total Score: ${quiz.score}</h2>`;
+    document.querySelector("#question").innerHTML = html;
+    let againButton = "<button id ='btnAgain' class ='btn btn-secondary'>Start Again</button>"
+    document.querySelector("#startAgain").innerHTML=againButton;
+    let btnAgain = document.getElementById("btnAgain");
+    btnAgain.onclick = function(){
+        btnAgain.style.display = "none";
+        quiz.questionIndex = 0;
+        questions = questions.sort(()=> Math.random()-0.5);
+        loadQuestion();
+    };
 };
 
 function showProgress(){
     let total = quiz.questions.length;
     let questionNumber = quiz.questionIndex+1;
-    document.querySelector("#progress").innerHTML =  "Question "+ questionNumber + " of " + total;
+    document.querySelector("#progress").innerText =  "Question "+ questionNumber + " of " + total;
 };
 
-
-
-
-
-
-
-//console.log(quiz.isFinish());
+loadQuestion();
 
 
 
